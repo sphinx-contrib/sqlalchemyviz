55c3191d4bbf594d85023e0f748553e62ab12e9a
Chintalagiri Shashank <shashank@chintal.in>
2016-01-10 01:02:48 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top
+# level README file and 2) it's easier to type in the README file than to
+# put a raw string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+requires = ['Sphinx>=0.6',
+            'SQLAlchemyViz']
+
+setup(
+    name='sphinxcontrib-sqlalchemyviz',
+    version='0.1',
+    url='https://github.com/chintal/sphinxcontrib-sqlalchemyviz',
+    download_url='https://github.com/chintal/sphinxcontrib-sqlalchemyviz',
+    license='BSD',
+    author='Chintalagiri Shashank',
+    author_email='shashank@chintal.in',
+    description='Sphinx SQLAlchemyViz extension',
+    long_description=read('README.rst'),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Framework :: Sphinx :: Extension',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+)
---
+++ sphinxcontrib/sqlalchemyviz.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016, Chintalagiri Shashank
+#
+# This Sphinx Extension is made available under the BSD 2-clause License. See
+# sphinxcontrib's LICENSE file for the full text.
+
+import os
+import shutil
+import subprocess
+
+from docutils import nodes
+from docutils.parsers.rst import directives
+from docutils.parsers.rst.directives.images import Image
+
+
+class SQLAlchemyViz(Image):
+
+    option_spec = {'metadataobject': directives.unchanged}
+    required_arguments = 0
+
+    def run(self):
+        if self.content:
+            error = self.state_machine.reporter.error(
+                """The SQLAlchemyViz directive does not know what to
+                do with provided content""",
+                nodes.literal_block(self.block_text, self.block_text),
+                line=self.lineno
+            )
+            return [error]
+
+        env = self.state.document.settings.env  # sphinx.environment.BuildEnvironment
+        config = env.config                     # sphinx.config.Config
+        output_folder = os.path.abspath(
+                os.path.join(env.srcdir, config["html_static_path"][0])
+        )
+
+        # Get the path to the object containing the metadata
+        metadatapath = self.options.get('metadataobject', None)
+        if metadatapath is None:
+            error = self.state_machine.reporter.error(
+                "SQLAlchemyViz needs to be given the object"
+                "containing the SQLAlchemy metadata as the :metadataobject: "
+                "parameter",
+                nodes.literal_block(self.block_text, self.block_text),
+                line=self.lineno
+            )
+            return [error]
+        output_path = os.path.join(output_folder, metadatapath + '.png')
+        metadata = ':'.join(metadatapath.rsplit('.', 1))
+        sqlaviz_cmd = ['sqlaviz',
+                       '--unique-relations',
+                       '--show-constraints',
+                       metadata,
+                       '-f out.dot']
+
+        devnull = open(os.devnull)
+
+        sqlaviz_sp = subprocess.Popen(' '.join(sqlaviz_cmd),
+                                      stdout=devnull,
+                                      stderr=devnull,
+                                      shell=True)
+        sqlaviz_sp.communicate()
+
+        render_cmd = 'ccomps -x out.dot | dot | gvpack -array3 ' \
+                     '| neato -Tpng -n2 -o out.png'
+        render_sp = subprocess.Popen(render_cmd,
+                                     stdout=devnull,
+                                     stderr=devnull,
+                                     shell=True)
+        render_sp.communicate()
+        devnull.close()
+
+        os.remove('out.dot')
+        shutil.move('out.png', output_path)
+
+        relpath = os.path.relpath(output_path, env.srcdir)
+
+        # I'm exhausted.
+        self.arguments.insert(0, '/' + relpath)
+
+        return Image.run(self)
+
+
+def setup(app):
+    app.add_directive('sqlaviz', SQLAlchemyViz)
+    return {'version': '0.1'}
